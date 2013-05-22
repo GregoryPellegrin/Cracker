1f0aa0348c8e4c15e426b1422e472ecdf6b2d6d8
GregoryPellegrin <pellegrin.gregory@gmail.com>
2013-05-21 23:32:48 -0400

    Jeu

--- Cracker/src/main/java/Cracker/Cracker.java
+++ Cracker/src/main/java/Cracker/Cracker.java
@@ -1,13 +1,14 @@
-package Cracker;
-
-import java.io.IOException;
-
 /*
  * Gregory Pellegrin
  * pellegrin.gregory.work@gmail.com
  * 
  * Trouve le mot de passe d'une archive .zip ou .rar
  */
+
+package Cracker;
+
+import java.io.IOException;
+
 public final class Cracker
 {
 	private char dictionaryAll [] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};

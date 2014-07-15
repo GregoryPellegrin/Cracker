586010e843ecca4d1e3d3be4add168fe1753d737
Gregory <Gregory@hyperion>
2014-07-15 12:33:20 -0400

    Update

--- Cracker/src/main/java/Cracker/Cracker.java
+++ Cracker/src/main/java/Cracker/Cracker.java
@@ -11,23 +11,20 @@ import java.io.IOException;
 
 public final class Cracker
 {
-	private char dictionaryAll [] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	private char dictionaryNumber [] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-	private char dictionaryTextAll [] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	private char dictionaryTextLower [] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	private char dictionaryTextUpper [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	private final char dictionaryAll [] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	private final char dictionaryNumber [] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	private final char dictionaryTextAll [] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	private final char dictionaryTextLower [] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	private final char dictionaryTextUpper [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 	private char dictionary [];
-	private String pathFile;
+	private final String pathFile;
 	private String command;
 	private double oldTime;
-	private int minLengthPassword;
-	private int maxLengthPassword;
-	private int displayTime;
+	private final int minLengthPassword;
+	private final int maxLengthPassword;
+	private final int displayTime;
 	private int totalFrame;
 	private int frame;
-	
-	public static final int ZIP = 0;
-	public static final int RAR = 1;
 	
 	public static final int ALL = 0;
 	public static final int NUMBER = 1;
@@ -37,20 +34,19 @@ public final class Cracker
 	
 	/*
 	 * pathFile : Si chemin relatif, partir de Cracker/ ("./src/main/resources/CrackerPackage/fichier.zip")
-	 * typeFile : Extension du fichier (.zip) (.rar)
 	 * typeCrack : Caracteres du password (Chiffres + Minuscules + Majuscules) (Chiffres) (Minuscules + Majuscules) (Minuscules) (Majuscules)
 	 * minLengthPassword : Taille minimale du password (azerty = 1,2,3,4,5,6 minLengthPassword)
 	 * maxLengthPassword : Taille maximale du password (azerty = 6 maxLengthPassword)
 	 * (Calcul du password de minLengthPassword a maxLengthPassword)
 	 * displayTime : Temps d'apparition des statistiques (Millisecondes)
 	 */
-	public Cracker (String pathFile, int typeFile, int typeCrack, int minLengthPassword, int maxLengthPassword, int displayTime)
+	public Cracker (String pathFile, int typeCrack, int minLengthPassword, int maxLengthPassword, int displayTime)
 	{
 		this.pathFile = " " + pathFile;
 		
-		if (typeFile == Cracker.ZIP)
+		if (pathFile.endsWith(".zip"))
 			this.command = "unzip -P ";
-		else if (typeFile == Cracker.RAR)
+		else if (pathFile.endsWith(".rar"))
 			this.command = "unrar x -p";
 		
 		if (typeCrack == Cracker.ALL)
@@ -92,8 +88,6 @@ public final class Cracker
 	
 	private void execute (String password)
 	{
-		//System.out.println(this.command + password + this.pathFile);
-		
 		try
 		{
 			Runtime.getRuntime().exec(this.command + password + this.pathFile);
@@ -121,8 +115,22 @@ public final class Cracker
 	
 	public static void main (String [] args)
 	{
-		Cracker crack = new Cracker ("./src/main/resources/Cracker/test.zip", Cracker.ZIP, Cracker.TEXT_LOWER, 3, 4, 1000);
-		
-		crack.start();
+		System.out.println(args.length);
+		if (args.length == 5)
+		{
+			Cracker crack = new Cracker (args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]), Integer.parseInt(args[4]));
+			
+			crack.start();
+		}
+		else
+		{
+			System.out.println("1 : Nom du fichier");
+			System.out.println("2 : Choix du dictionnaire (0 Tout, 1 Nombre, 2 Lettres, 3 Minuscules, 4 Majuscules)");
+			System.out.println("3 : Taille minimum du mot de passe");
+			System.out.println("4 : Taille Maximum du mot de passe");
+			System.out.println("5 : Temps entre les affichages des statistiques");
+			
+			System.out.println("java -jar cracker \"fichier.zip|rar\" 3 4 6 100000");
+		}
 	}
 }
